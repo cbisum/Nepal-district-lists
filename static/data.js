61ad625c88cce8e832af24f7978b8a5144636f44
CB <cbshrestha24@gmail.com>
2022-09-10 00:31:21 +1000

    Init commit

---
+++ static/data.js
@@ -0,0 +1,464 @@
+const DATA = [
+    {
+      district_name: "Bhojpur",
+      total_area: "1,507",
+      headquarter_name: "Myanglung",
+      province_number: 1
+    },
+    {
+      district_name: "Dhankuta",
+      total_area: "892",
+      headquarter_name: "Dhankuta",
+      province_number: 1
+    },
+    {
+      district_name: "Ilam",
+      total_area: "1,703",
+      headquarter_name: "Ilam",
+      province_number: 1
+    },
+    {
+      district_name: "Jhapa",
+      total_area: "1,606",
+      headquarter_name: "Bhadrapur",
+      province_number: 1
+    },
+    {
+      district_name: "Khotang",
+      total_area: "1,591",
+      headquarter_name: "Diktel",
+      province_number: 1
+    },
+    {
+      district_name: "Morang",
+      total_area: "1,855",
+      headquarter_name: "Biratnagar",
+      province_number: 1
+    },
+    {
+      district_name: "Okhaldhunga",
+      total_area: "1,074",
+      headquarter_name: "Siddhicharan",
+      province_number: 1
+    },
+    {
+      district_name: "Panchthar",
+      total_area: "1,241",
+      headquarter_name: "Phidim",
+      province_number: 1
+    },
+    {
+      district_name: "Sankhuwasabha",
+      total_area: "3,480",
+      headquarter_name: "Khandbari",
+      province_number: 1
+    },
+    {
+      district_name: "Solukhumbu",
+      total_area: "3,312",
+      headquarter_name: "Salleri",
+      province_number: 1
+    },
+    {
+      district_name: "Sunsari",
+      total_area: "1,257",
+      headquarter_name: "Inaruwa",
+      province_number: 1
+    },
+    {
+      district_name: "Taplejung",
+      total_area: "3,646",
+      headquarter_name: "Taplejung",
+      province_number: 1
+    },
+    {
+      district_name: "Terhathum",
+      total_area: "679",
+      headquarter_name: "Myanglung",
+      province_number: 1
+    },
+    {
+      district_name: "Udayapur",
+      total_area: "2,063",
+      headquarter_name: "Gaighat",
+      province_number: 1
+    },
+    {
+      district_name: "Bara",
+      total_area: "1,190",
+      headquarter_name: "Kalaiya",
+      province_number: 2
+    },
+    {
+      district_name: "Dhanusa",
+      total_area: "1,180",
+      headquarter_name: "Janakpur",
+      province_number: 2
+    },
+    {
+      district_name: "Mahottari",
+      total_area: "1,002",
+      headquarter_name: "Jaleshwar",
+      province_number: 2
+    },
+    {
+      district_name: "Parsa",
+      total_area: "1,353",
+      headquarter_name: "Birgunj",
+      province_number: 2
+    },
+    {
+      district_name: "Rautahat",
+      total_area: "1,126",
+      headquarter_name: "Gaur",
+      province_number: 2
+    },
+    {
+      district_name: "Saptari",
+      total_area: "1,363",
+      headquarter_name: "Rajbiraj",
+      province_number: 2
+    },
+    {
+      district_name: "Sarlahi",
+      total_area: "1,259",
+      headquarter_name: "Malangwa",
+      province_number: 2
+    },
+    {
+      district_name: "Siraha",
+      total_area: "1,188",
+      headquarter_name: "Siraha",
+      province_number: 2
+    },
+    {
+      district_name: "Bhaktapur",
+      total_area: "119",
+      headquarter_name: "Bhaktapur",
+      province_number: 3
+    },
+    {
+      district_name: "Chitwan",
+      total_area: "2,218",
+      headquarter_name: "Bharatpur",
+      province_number: 3
+    },
+    {
+      district_name: "Dhading",
+      total_area: "1,926",
+      headquarter_name: "Nilkantha",
+      province_number: 3
+    },
+    {
+      district_name: "Dolakha",
+      total_area: "2,191",
+      headquarter_name: "Bhimeshwar",
+      province_number: 3
+    },
+    {
+      district_name: "Kathmandu",
+      total_area: "395",
+      headquarter_name: "Kathmandu",
+      province_number: 3
+    },
+    {
+      district_name: "Kavrepalanchok",
+      total_area: "1,396",
+      headquarter_name: "Dhulikhel",
+      province_number: 3
+    },
+    {
+      district_name: "Lalitpur",
+      total_area: "385",
+      headquarter_name: "Lalitpur",
+      province_number: 3
+    },
+    {
+      district_name: "Makwanpur",
+      total_area: "2,426",
+      headquarter_name: "Hetauda",
+      province_number: 3
+    },
+    {
+      district_name: "Nuwakot",
+      total_area: "1,121",
+      headquarter_name: "Bidur",
+      province_number: 3
+    },
+    {
+      district_name: "Ramechhap",
+      total_area: "1,546",
+      headquarter_name: "Manthali",
+      province_number: 3
+    },
+    {
+      district_name: "Rasuwa",
+      total_area: "1,544",
+      headquarter_name: "Dhunche",
+      province_number: 3
+    },
+    {
+      district_name: "Sindhuli",
+      total_area: "2,491",
+      headquarter_name: "Kamalamai",
+      province_number: 3
+    },
+    {
+      district_name: "Sindhupalchok",
+      total_area: "2,542",
+      headquarter_name: "Chautara",
+      province_number: 3
+    },
+    {
+      district_name: "Baglung",
+      total_area: "1,784",
+      headquarter_name: "Baglung",
+      province_number: 4
+    },
+    {
+      district_name: "Gorkha",
+      total_area: "3,610",
+      headquarter_name: "Gorkha",
+      province_number: 4
+    },
+    {
+      district_name: "Kaski",
+      total_area: "2,017",
+      headquarter_name: "Pokhara",
+      province_number: 4
+    },
+    {
+      district_name: "Lamjung",
+      total_area: "1,692",
+      headquarter_name: "Besisahar",
+      province_number: 4
+    },
+    {
+      district_name: "Manang",
+      total_area: "2,246",
+      headquarter_name: "Chame",
+      province_number: 4
+    },
+    {
+      district_name: "Mustang",
+      total_area: "3,573",
+      headquarter_name: "Jomsom",
+      province_number: 4
+    },
+    {
+      district_name: "Myagdi",
+      total_area: "2,297",
+      headquarter_name: "Beni",
+      province_number: 4
+    },
+    {
+      district_name: "Nawalpur",
+      total_area: "1,043.10",
+      headquarter_name: "Kawasoti",
+      province_number: 4
+    },
+    {
+      district_name: "Parbat",
+      total_area: "494",
+      headquarter_name: "Kusma",
+      province_number: 4
+    },
+    {
+      district_name: "Syangja",
+      total_area: "1,164",
+      headquarter_name: "Putalibazar",
+      province_number: 4
+    },
+    {
+      district_name: "Tanahun",
+      total_area: "1,546",
+      headquarter_name: "Damauli",
+      province_number: 4
+    },
+    {
+      district_name: "Arghakhanchi",
+      total_area: "1,193",
+      headquarter_name: "Sandhikharka",
+      province_number: 5
+    },
+    {
+      district_name: "Banke",
+      total_area: "2,337",
+      headquarter_name: "Nepalganj",
+      province_number: 5
+    },
+    {
+      district_name: "Bardiya",
+      total_area: "2,025",
+      headquarter_name: "Gulariya",
+      province_number: 5
+    },
+    {
+      district_name: "Dang",
+      total_area: "2,955",
+      headquarter_name: "Ghorahi",
+      province_number: 5
+    },
+    {
+      district_name: "Eastern Rukum",
+      total_area: "1,161.13",
+      headquarter_name: "Rukumkot",
+      province_number: 5
+    },
+    {
+      district_name: "Gulmi",
+      total_area: "1,149",
+      headquarter_name: "Tamghas",
+      province_number: 5
+    },
+    {
+      district_name: "Kapilvastu",
+      total_area: "1,738",
+      headquarter_name: "Taulihawa",
+      province_number: 5
+    },
+    {
+      district_name: "Palpa",
+      total_area: "1,373",
+      headquarter_name: "Tansen",
+      province_number: 5
+    },
+    {
+      district_name: "Parasi",
+      total_area: "634.88",
+      headquarter_name: "Ramgram",
+      province_number: 5
+    },
+    {
+      district_name: "Pyuthan",
+      total_area: "1,309",
+      headquarter_name: "Pyuthan",
+      province_number: 5
+    },
+    {
+      district_name: "Rolpa",
+      total_area: "1,879",
+      headquarter_name: "Liwang",
+      province_number: 5
+    },
+    {
+      district_name: "Rupandehi",
+      total_area: "1,360",
+      headquarter_name: "Siddharthanagar",
+      province_number: 5
+    },
+    {
+      district_name: "Dailekh",
+      total_area: "1,502",
+      headquarter_name: "Narayan",
+      province_number: 6
+    },
+    {
+      district_name: "Dolpa",
+      total_area: "7,889",
+      headquarter_name: "Dunai",
+      province_number: 6
+    },
+    {
+      district_name: "Humla",
+      total_area: "5,655",
+      headquarter_name: "Simikot",
+      province_number: 6
+    },
+    {
+      district_name: "Jajarkot",
+      total_area: "2,230",
+      headquarter_name: "Khalanga",
+      province_number: 6
+    },
+    {
+      district_name: "Jumla",
+      total_area: "2,531",
+      headquarter_name: "Chandannath",
+      province_number: 6
+    },
+    {
+      district_name: "Kalikot",
+      total_area: "1,741",
+      headquarter_name: "Manma",
+      province_number: 6
+    },
+    {
+      district_name: "Mugu",
+      total_area: "3,535",
+      headquarter_name: "Gamgadhi",
+      province_number: 6
+    },
+    {
+      district_name: "Salyan",
+      total_area: "1,462",
+      headquarter_name: "Salyan",
+      province_number: 6
+    },
+    {
+      district_name: "Surkhet",
+      total_area: "2,451",
+      headquarter_name: "Birendranagar",
+      province_number: 6
+    },
+    {
+      district_name: "Western Rukum",
+      total_area: "1,213.49",
+      headquarter_name: "Musikot",
+      province_number: 6
+    },
+    {
+      district_name: "Achham",
+      total_area: "1,680",
+      headquarter_name: "Mangalsen",
+      province_number: 7
+    },
+    {
+      district_name: "Baitadi",
+      total_area: "1,519",
+      headquarter_name: "Dasharathchand",
+      province_number: 7
+    },
+    {
+      district_name: "Bajhang",
+      total_area: "3,422",
+      headquarter_name: "Jayaprithvi",
+      province_number: 7
+    },
+    {
+      district_name: "Bajura",
+      total_area: "2,188",
+      headquarter_name: "Martadi",
+      province_number: 7
+    },
+    {
+      district_name: "Dadeldhura",
+      total_area: "1,538",
+      headquarter_name: "Amargadhi",
+      province_number: 7
+    },
+    {
+      district_name: "Darchula",
+      total_area: "2,322",
+      headquarter_name: "Darchula",
+      province_number: 7
+    },
+    {
+      district_name: "Doti",
+      total_area: "2,025",
+      headquarter_name: "Dipayal Silgadhi",
+      province_number: 7
+    },
+    {
+      district_name: "Kailali",
+      total_area: "3,235",
+      headquarter_name: "Dhangadhi",
+      province_number: 7
+    },
+    {
+      district_name: "Kanchanpur",
+      total_area: "1,610",
+      headquarter_name: "Bhimdatta",
+      province_number: 7
+    }
+   ]
